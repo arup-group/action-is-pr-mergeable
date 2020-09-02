9a56efdac172e52c47c6cbea73d621c6e1021aa8
peterjgrainger <peter.grainger@arup.com>
2020-09-02 15:18:25 +0100

    initial version

---
+++ src/logic.ts
@@ -0,0 +1,20 @@
+import {ActionContext} from './action-context'
+
+export async function isMergable(actionContext: ActionContext): Promise<void> {
+  try {
+    if (!actionContext.context.payload.pull_request)
+      throw Error('This action only works if the trigger is a pull request')
+
+    const pullRequest = await actionContext.octokit.pulls.get({
+      ...actionContext.context.repo,
+      pull_number: actionContext.context.payload.pull_request.number
+    })
+
+    actionContext.setOutput(
+      'mergable',
+      pullRequest.data.mergeable ? 'true' : 'false'
+    )
+  } catch (error) {
+    actionContext.setFailed(error.message)
+  }
+}
